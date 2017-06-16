e3095e9dbc91f9c410d35ee492f0b134fa6ad536
Ozan Araz <ozanaraz91@Ozans-MacBook-Pro.local>
2017-06-16 15:57:58 +0200

    title

---
+++ imports/api/Questions.tests.js
@@ -0,0 +1,38 @@
+import { Meteor } from 'meteor/meteor';
+import expect from 'expect';
+
+import { Questions } from './Questions';
+
+if (Meteor.isServer) {
+  describe('questions', function () {
+    const firstQ = {
+      _id: '1',
+      question: 'What is the capital of Spain ?',
+      "responses" : [ "Madrid", "Malaga", "Almeria", "Barcelona" ],
+      "feedbacks" : [ { "response" : "Madrid", "count" : 9 },
+                      { "response" : "Malaga", "count" : 3 },
+                      { "response" : "Almeria", "count" : 2 },
+                      { "response" : "Barcelona", "count" : 3 } ]
+    };
+    const secondQ = {
+      _id: '2',
+      "responses" : [ "Paris", "Nice", "Narbonne", "Lyon" ],
+      "feedbacks" : [ { "response" : "Paris", "count" : 51 },
+                      { "response" : "Nice", "count" : 34 },
+                      { "response" : "Narbonne", "count" : 32 },
+                      { "response" : "Lyon", "count" : 21} ]
+    };
+
+    beforeEach(function () {
+     Questions.remove({});
+     Questions.insert(firstQ);
+     Questions.insert(secondQ);
+   });
+
+
+   it('Should insert new questions to the database', function () {
+     expect(Questions.findOne({_id : '1'})).toExist();
+     expect(Questions.findOne({_id : '2'})).toExist();
+   });
+ });
+}
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render(){
     return(
       <div>
-        <Header title='Question Checkbox'/>
+        <Header title='Quantilope'/>
         <div className="wrapper">
           <AddQuestion />
         </div>
