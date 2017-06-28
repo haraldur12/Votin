e9a1abef9adac6f5f49b816fe50f0637a3574c73
Ozan Araz <ozanaraz91@Ozans-MacBook-Pro.local>
2017-06-28 16:10:00 +0200

    stateless components

--- imports/ui/Charts/ViewData.js
+++ imports/ui/Charts/ViewData.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Header from './../Components/Header'
+import Header from './../Components/Header';
 import ChartIndex from './ChartIndex';
-
-export default class ViewData extends Component {
-  render() {
-    return (
-      <div>
-        <Header title="Showing results for your question" />
-        <ChartIndex id={this.props.match.params.id} />
-      </div>
-    )
-  }
+// with arrow functions it does not access the params.id
+export default function ViewData(props) {
+  return (
+    <div>
+      <Header title="Showing results for your question" />
+      <ChartIndex id={props.match.params.id} />
+    </div>
+  );
 }
+
+ViewData.propTypes = {
+  match: PropTypes.object
+};
